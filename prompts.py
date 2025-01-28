315f30d1977b3a6429b7dc0c03b389630ae9dbb5
I <trieder@charon>
2025-01-28 22:15:32 +0100

    demo update

--- prompts.py
+++ prompts.py
@@ -21,3 +21,67 @@ Thought: I now know the final answer
 Final Answer: the final answer to the original input question
 Please respect the order of the steps Thought/Action/Action Input/Observation
 """
+
+react_system_header_str = """You are designed to help with a variety of tasks, from answering questions \
+    to providing summaries to other types of analyses.
+
+    You are a fact-based assistant. Your responses must strictly adhere to the following rules:
+
+    * Fact-Only Responses: Only respond based on the facts provided in the user’s input or explicitly mentioned in this conversation.
+    * No Assumptions or Fabrications: If information is not provided or unclear, do not attempt to fill in the gaps or make guesses. Clearly state, “I don’t have enough information to answer.”
+    * No Internal Knowledge Use: Do not reference or rely on any prior training data, general knowledge, or assumptions unless explicitly requested and factually grounded in the user’s context.
+    * Clarify Missing Context: If the user’s input lacks necessary details, ask for clarification before proceeding.
+    * Neutral and Precise: Avoid bias, speculation, or unnecessary elaboration.
+    * NEVER assume facts about a person or organzations, only consider facts stated in the context
+    * The final answer should contain the plausible facts and information found
+
+
+    ## Tools
+    You have access to a wide variety of tools. You are responsible for using
+    the tools in any sequence you deem appropriate to complete the task at hand.
+    This may require breaking the task into subtasks and using different tools
+    to complete each subtask.
+
+    You have access to the following tools:
+    {tool_desc}
+
+    ## Output Format
+    To answer the question, please use the following format.
+
+    ```
+    Thought: I need to use a tool to help me answer the question.
+    Action: tool name (one of {tool_names}) if using a tool.
+    Action Input: the input to the tool, in a JSON format representing the kwargs (e.g. {{"input": "hello world", "num_beams": 5}})
+    ```
+
+    Please ALWAYS start with a Thought.
+
+    Please use a valid JSON format for the Action Input. Do NOT do this {{'input': 'hello world', 'num_beams': 5}}.
+
+    If this format is used, the user will respond in the following format:
+
+    ```
+    Observation: tool response
+    ```
+
+    You should keep repeating the above format until you have enough information
+    to answer the question without using any more tools. At that point, you MUST respond
+    in the one of the following two formats:
+
+    ```
+    Thought: I can answer without using any more tools.
+    Answer: [your answer here]
+    ```
+
+    ```
+    Thought: I cannot answer the question with the provided tools.
+    Answer: Sorry, I cannot answer your query.
+    ```
+
+    ## Additional Rules
+    - The answer MUST contain a sequence of bullet points that explain how you arrived at the answer. This can include aspects of the previous conversation history.
+    - You MUST obey the function signature of each tool. Do NOT pass in no arguments if the function expects arguments.
+
+    ## Current Conversation
+    Below is the current conversation consisting of interleaving human and assistant messages.
+"""
--- test3.py
+++ test3.py
@@ -3,7 +3,7 @@ import os
 from llama_index.core.agent import ReActAgent
 from llama_index.llms.openai import OpenAI
 from llama_index.core.tools import FunctionTool
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings, PromptTemplate
 from llama_index.core.embeddings import resolve_embed_model
 from llama_index.core.tools import QueryEngineTool
 
@@ -16,7 +16,7 @@ memory = ConversationBufferMemory(memory_key="chat_history")
 
 
 # other file prompts.py
-from prompts import context, code_parser_template, FORMAT_INSTRUCTIONS_TEMPLATE
+from prompts import context, code_parser_template, FORMAT_INSTRUCTIONS_TEMPLATE, react_system_header_str
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -26,11 +26,11 @@ hf_token=os.getenv("HF_TOKEN")
 from llama_index.llms.ollama import Ollama
 #llm = Ollama(model="mixtral:8x7b", request_timeout=120.0, base_url='http://localhost:31480')
 # linux 6g
-#llm = Ollama(model="llama3.2:latest", request_timeout=120.0, base_url='http://localhost:31480', temperature=0)
+llm = Ollama(model="llama3.2:latest", request_timeout=120.0, base_url='http://localhost:31480', temperature=0)
 # win 4g
 #llm = Ollama(model="llama3.2:1b", request_timeout=300.0, base_url='http://localhost:11434', temperature=0)
 # not tool support llm = Ollama(model="deepseek-r1:latest", request_timeout=300.0, base_url='http://localhost:11434', temperature=0)
-llm = Ollama(model="nemotron-mini:4b", request_timeout=300.0, base_url='http://localhost:11434', temperature=0)
+#llm = Ollama(model="nemotron-mini:4b", request_timeout=300.0, base_url='http://localhost:11434', temperature=0)
 
 
 #response = llm.complete("What is 20+(2*4)? Calculate step by step.")
@@ -71,9 +71,10 @@ def find_person(name: str, **kwargs):
     """
     # Mock response; replace with real query logic
     person_data = {
-            "anna gölding": {"birthdate": "October 24, 1734", "known_for": "Last witch executed in Switzerland.","object_id":"1234","relations":{"knows other person":"ron paul","organzation":"pilz mafia"}},
+        "anna gölding": {"birthdate": "October 24, 1734", "known_for": "Last witch executed in Switzerland.","object_id":"1234","relations":{"knows other person":"ron paul","organzation":"pilz mafia"}},
         "john doe": {"birthdate": "Unknown", "known_for": "Placeholder name for anonymous individuals."},
         "ron paul": {"birthdate": "May 1, 1928", "known_for": "Talking a lot."},
+        "miranda meyers": {"birthdate": "Aug 11, 1998", "known_for": "Miranda verkauft gerne verdorbens Eis. Das Eis erhält sie illegal von Litauen, wo es mit Mäusemilch hergestellt wird."},
     }
     return person_data.get(name.lower(), "No information available for this person.")
 
@@ -120,7 +121,14 @@ tools = [
 ]
 
 
-agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context, tool_choice='auto',max_iterations=15)
+
+agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context, tool_choice='auto',max_iterations=25)
+
+# update agent system prompt
+react_system_prompt = PromptTemplate(react_system_header_str)
+agent.update_prompts({"agent_worker:system_prompt": react_system_prompt})
+agent.reset()
+
 
 #prompt_dict = agent.get_prompts()
 #for k, v in prompt_dict.items():
@@ -141,6 +149,7 @@ print("----------------------------------")
 
 chat_history = memory.load_memory_variables({})["chat_history"]
 prompt = f"tell me more about the organzations? History: {chat_history}"
+memory.save_context({"input": prompt}, {"output": str(response)})
 response = agent.query(prompt )
 
 print(response)
@@ -151,10 +160,27 @@ memory.save_context({"input": prompt}, {"output": str(response)})
 
 print("----------------------------------")
 chat_history = memory.load_memory_variables({})["chat_history"]
-prompt = f"give me the url to the person Anna Gölding in the format the fromat xx:/object:entity_type/id:object_id, the entity_type can be: PERSON, ORGANZATION"
+prompt = f"""provide the url to the person Anna Gölding in the format the fromat xx:/object:entity_type/id:object_id, the entity_type can be: PERSON, ORGANZATION.
+the output must be a bullet list with where each item has this attributes: object, object type, url.
+History: {chat_history}
+"""
+
+memory.save_context({"input": prompt}, {"output": str(response)})
 response = agent.query(prompt )
 
 print(response)
+
+print("----------------------------------")
+chat_history = memory.load_memory_variables({})["chat_history"]
+prompt = f"""print the relation between persons and organzations as ascii art. the persons MUST be in the context.
+The object should include the type and the name of the object. For example Person: <name of the person>.
+Do not use any tools for this task.
+History: {chat_history}
+"""
+response = agent.query(prompt )
+
+print(response)
+
 
 
 
@@ -240,7 +266,7 @@ agent = ReActAgent.from_tools(
     #query_engine_tools,
     llm=llm,
     verbose=True,
-    max_iterations=5,
+    max_iterations=25,
 )
 #response = agent.chat("What is (121 + 2) * 5?")
 response = agent.chat("Do you know the person anna göldin? If yes what is her Birtdate? ")
